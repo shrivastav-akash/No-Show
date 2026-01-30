97a1fd0119e85dac2ebe83f908c4f51eeffff6db
Akash Shrivastav <shrivastav.work@gmail.com>
2026-01-30 22:49:31 +0530

    updated .env

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/attendance-app')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error(err));
 
