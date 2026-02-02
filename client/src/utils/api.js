3af645609dceb2e2ee0db1d567bb8a486d8be6e7
Akash Shrivastav <shrivastav.work@gmail.com>
2026-02-02 10:25:38 +0530

    deployment changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,22 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+require("dotenv").config();
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 // Interceptor to add token
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers['x-auth-token'] = token;
+      config.headers["x-auth-token"] = token;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default api;
