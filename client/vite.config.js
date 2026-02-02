aac612728e5d05a11480d70b91d6c2a62422dfd8
Akash Shrivastav <shrivastav.work@gmail.com>
2026-02-02 12:54:04 +0530

    deployment configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://attendify-x39n.onrender.com/',
         changeOrigin: true,
         secure: false,
       }
